e4c6c30d129dc5489150732d7c06752a1817d679
marcelo.bdamaceno <marcelo.bdamaceno@CAS419190C14722.senacsp.edu.br>
2015-08-14 20:48:54 -0300

    Alteração

--- src/main/java/br/senac/tads/Exercicio01.java
+++ src/main/java/br/senac/tads/Exercicio01.java
@@ -14,4 +14,8 @@ public class Exercicio01 {
     public static void main(String[] args) {
         System.out.println("Teste");
     }
+    public String Teste(){
+        System.out.println("Teste2");
+        return null;
+    }
 }
